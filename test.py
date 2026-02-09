5d2a7d963c3e9e34e0b9b34bdbec0c89c60c3814
matartiflette <maxence.tardivel.mt@gmail.com>
2026-02-09 23:56:27 +0100

    test

--- test.py
+++ test.py
@@ -1,15 +1,26 @@
 import dearpygui.dearpygui as dpg
-
+from graphe import graphe
 dpg.create_context()
-dpg.create_viewport(title='Custom Title', width=600, height=300)
 
-with dpg.window(label="Example Window"):
-    dpg.add_text("Hello, world")
-    dpg.add_button(label="Save")
-    dpg.add_input_text(label="string", default_value="Quick brown fox")
-    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+slider_ids = []
 
+with dpg.window(label="Exemple"):
+    # Création de plusieurs sliders
+    slider_ids.append(dpg.add_slider_int(label="nombre de personnes", default_value=20, max_value=50))
+    slider_ids.append(dpg.add_slider_float(label="Slider 2", default_value=0.5, max_value=1.0))
+    slider_ids.append(dpg.add_slider_float(label="Slider 3", default_value=0.8, max_value=1.0))
+    
+    def start_callback():
+        valeurs = [dpg.get_value(s) for s in slider_ids]
+        print("Valeurs des sliders au Start :", valeurs)
+    
+    dpg.add_button(label="Start", callback=start_callback)
+
+dpg.create_viewport(title='Demo', width=400, height=200)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
 dpg.destroy_context()
+
+
+slider_ids[0]
