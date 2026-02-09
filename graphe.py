9b5841c6cddb9d24e22e09e645741ccff8066d73
Eliott559 <eliott.leborgne@icloud.com>
2026-02-09 21:49:44 +0100

    v2

--- graphe.py
+++ graphe.py
@@ -2,12 +2,15 @@ class graphe:
     def __init__(self, sommet):
         self.neighbors = dict() ## dico regroupant tous les sommets du graphe, clés sont les sommets et valeur un dico voir plus bas 
         for s in sommet:
-            self.neighbors[s] = [False] ## clés sont les voisins et valeurs les poids de la relation, plus le poids est important plus la proba de contaminer est importante
+            self.neighbors[s] = [False] ## clés sont les voisins et valeurs une liste format [True/False, [voisins]]
+
+    def append_neighbors(self, s, neighbors):
+        self.neighbors[s].append(neighbors)
                                 
-    def neighbors(s):
+    def neighbors(self, s):
         return self.neighbors[s][1]  ## renvoie la liste des voisins du sommet 
     
-    def is_neighbors(s1, s2):
+    def is_neighbors(self, s1, s2):
         return s1 in self.neighbors[s2][1]
     
     def __str__(self):
@@ -15,4 +18,6 @@ class graphe:
     
 
 bite = graphe(["A", "B", "C"])
+print(bite)
+bite.append_neighbors("A", ["B", "C"])
 print(bite)
